995d79ebde58a3803975747f7426635d39620a5f
AlenaMearbhall <89631576+AlenaMearbhall@users.noreply.github.com>
2022-10-18 20:38:06 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -22,15 +22,6 @@
             }));
         }
     }), 0);
-    $(document).ready((function() {
-        $("#menu").on("click", "a", (function(event) {
-            event.preventDefault();
-            var id = $(this).attr("href"), top = $(id).offset().top;
-            $("body,html").animate({
-                scrollTop: top
-            }, 1500);
-        }));
-    }));
     window["FLS"] = true;
     isWebp();
 })();
